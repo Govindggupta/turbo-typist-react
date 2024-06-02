6d728a1177c899395c30fed7a0e84d03b79c5e58
Govind Gupta <govind12771277@gmail.com>
2024-06-02 12:09:14 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TypingApp from './TypingApp';
+import './index.css'; // Ensure you import the CSS
+
+function App() {
+  return (
+    <div className="bg-[#2f2f2f] min-h-screen flex flex-col items-center">
+      <TypingApp />
+    </div>
+  );
+}
+
+export default App;
